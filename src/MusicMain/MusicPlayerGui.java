21e6cce8de788b5e4f36c3ca62670dcef1b71a96
Apichaya <apichayapg@gmail.com>
2017-12-11 16:52:53 +0700

    initial commit

---
+++ src/MusicMain/MusicPlayerGui.java
@@ -0,0 +1,17 @@
+package MusicMain;
+
+import javax.swing.*;
+
+public class MusicPlayerGui {
+    JButton play = new JButton("Play");
+    JButton stop = new JButton("Stop");
+    JButton pause = new JButton("Pause");
+
+    public static void main(String[] why){
+        new MusicPlayerGui().createGui();
+    }
+
+    public void createGui(){
+
+    }
+}
